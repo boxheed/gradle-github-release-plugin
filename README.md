b73ac2a416d1de3d9ad4a8dfd97e9581914009bf
boxheed <boxheed@users.noreply.github.com>
2023-05-30 23:19:35 +0100

    more initial changes

---
+++ README.md
@@ -0,0 +1,4 @@
+[![CircleCI](https://circleci.com/gh/boxheed/gradle-extended-info-plugin/tree/master.svg?style=shield)](https://circleci.com/gh/boxheed/gradle-extended-info-plugin/tree/master)
+
+# Gradle Github Release plugin
+A plugin for creating a release on Github
---
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/githubrelease/GithubReleasePlugin.groovy
@@ -0,0 +1,15 @@
+package com.fizzpod.gradle.plugins.githubrelease;
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+public class GithubReleasePlugin implements Plugin<Project> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GithubReleasePlugin.class);
+
+	void apply(Project project) {
+		
+	}
+}
